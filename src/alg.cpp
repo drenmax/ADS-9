9668507c8c5abc1ccfbd954ea7a8b91f64557e21
Дреняев Максим <drenyaev.max@gmail.com>
2022-05-26 20:29:25 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,5 +6,21 @@
 #include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+  BST<std::string> BSTree;
+  std::string Single_Word = "";
+  std::ifstream file(filename);
+  if (!file) throw std::string("File error!");
+  
+  while (!file.eof()) {
+    char letter = file.get();
+    if ((letter >= 'a' && letter <= 'z') || (letter >= 'A' && letter <= 'Z')) {
+      Single_Word += tolower(letter);
+    } else {
+      BSTree.add(Single_Word);
+      Single_Word = "";
+    }
+  }
+
+  file.close();
+  return BSTree;
 }
